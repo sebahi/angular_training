2bbda3bf9b09908dd6bb44db220eefe0270e2fe9
sebahi <sebahi.omar@gmail.com>
2019-06-09 03:15:40 -0400

    configured material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app',
+  template: `<div class="md-padding p-2 text-center text-white">
+              This is SportsStore
+              <button mat-button>Click Me!</button>
+            </div>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
---
+++ src/app/material.ts
@@ -0,0 +1,10 @@
+import { MatButtonModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [MatButtonModule],
+    exports: [MatButtonModule]
+})
+export class MaterialModule {
+
+}
